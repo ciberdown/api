fca2017bc0846a7938b8aaa1ca499fb85f127c88
teymuri <teymuri.amin@gmail.com>
2024-07-02 06:53:12 +0330

    chore

--- src/Interfaces/IStudentRepo.cs
+++ src/Interfaces/IStudentRepo.cs
@@ -7,7 +7,7 @@ namespace api.Interfaces
     public interface IStudentRepo
     {
         public Task<List<Student>?> Get(StudentQueryObject query);
-        public Task<StudentDto?> GetById(int id);
+        public Task<Student?> GetById(int id);
         public Task<Student?> Create(CreateStudentDto createStudentDto);
         public Task<bool> Delete(int id);
         public Task<Student?> Update(int id, UpdateStudentDto updateStudentDto);
